5ce203ab29c994fc48d50a5bd17c03813d406ede
Bethel Leya Tesfamariam Woldu <b.woldu@se23.qmul.ac.uk>
2025-03-22 20:11:34 +0000

    login and register

--- login.php
+++ login.php
@@ -1,58 +1,79 @@
 <?php
+
 session_start();
 
-echo 'Hello';
+include "connection.php";
 
-include 'connection.php';
+// imports PHPMailer classes
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-if (isset($_POST['email']) && isset($_POST['password'])) {
-    function validate($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
+// load PHPMailer files
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
 
-$email = validate($_POST['email']);
-$password = validate($_POST['password']);
+// checks if form submitted
+if ($_SERVER["REQUEST_METHOD"]) {
+    // gets data from form
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $role = $_POST['role'];
 
-if (empty($email) || empty($password)){
-    header("Location: login.php?error=Email and Password is required");
-    exit();
-}
+    $_SESSION['email'] = $email;
 
-$sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password' ";
-$result = $conn->query($sql);
+    // sql query to find user with the provided email and role
+    $sql = "SELECT * FROM fdm_users WHERE email = '$email' AND role = '$role'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
 
-if($result->num_rows ===1){
-    $row = $result->fetch_assoc();
+    $_SESSION['role'] = $row['role'];
 
+    // checks if user exists and password is correct
+    if ($row && password_verify($password, $row['password'])) {
+        // otp expiry datetime created - 5minutes
+        $otp_expiry = date('Y-m-d H:i:s', strtotime('+5 minutes'));
 
-    if ($row['email'] === $email && $row['password'] === $password) {
-        $_SESSION['email'] = $row['email'];
-        $_SESSION['password'] = $row['password'];
-        $_SESSION['role'] = $row['role'];
+        // email details and message
+        $subject = "One Time Password";
+        $otp = rand(100000, 999999);
+        $message = "Your OTP is: " . $otp;
 
-    if ($row['role'] === 'consultant')
-    {
-        header ("Location: consultantHomePage.html");
+        // new instance of PHPMailer, and SMTP used to send email
+        $mail = new PHPMailer(true);
+        $mail->isSMTP(); 
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'fdm.otp@gmail.com'; // host email
+        $mail->Password = 'bjdl qnwm cpis kxlu'; // host passwords - connects to SMTP server
+        $mail->Port = 465; 
+        $mail->SMTPSecure = 'ssl';
+        $mail->isHTML(true);
+
+        // sender and receiver email
+        $mail->setFrom('fdm.otp@gmail.com', 'FDM Flat Finder OTP');
+        $mail->addAddress($email);
+
+        // email subject and message
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+        $mail->send(); //sends email
+
+        // updates user otp and otp expiry in database
+        $sql = "UPDATE fdm_users SET otp = '$otp', otp_expiry = '$otp_expiry' WHERE email = '$email'";
+        $result = mysqli_query($conn, $sql);
+
+        //user redirected to the otp verification page
+        header("Location: otp.html");
         exit();
-    } 
-    else if ($row['role'] === 'landlord')
-    {
-        header ("Location: landlordHomePage.html");
-        exit();
-    }
-}
-    else{
-        header("Location: login.php?error=Incorrect Email or Password");
-        exit();
-    }
-}
-else{
-    header("Location: login.php?error=Incorrect Email or Password");
-    exit();
-} 
 
+        //error message if email or password is incorrect
+    } else {
+        echo '<script>
+        alert("Invalid email or password. Please try again.");
+        window.location.href = "loginConsultant.html";
+        </script>';
+        exit();
+    }
 }
-?>
+
