f920530e36f96918b7f1260f2cf96eb3a5ee4dce
Riaz Rahman <r.rahman@se23.qmul.ac.uk>
2025-03-31 22:43:18 +0100

    added connection file for edit preferences

---
+++ connection_preferences.php
@@ -0,0 +1,13 @@
+<?php
+
+$sname = "localhost";
+$db_email = "root";
+$db_password = "";
+$db_name = "FDMFlatFinder";
+
+$conn = mysqli_connect($sname, $db_email, $db_password, $db_name);
+
+if (!$conn) {
+    echo "Connection failed!" . mysqli_connect_error();
+}
+?>
