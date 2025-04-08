472fd3ef78ecfb45ed83c10dd6d99841701946d4
RevisionAaron <aaronparwany.p@gmail.com>
2025-04-08 02:26:32 +0100

    changed account.html to account.php

--- otp.php
+++ otp.php
@@ -47,10 +47,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 // redirect to pages depending on role
                 if (isset($_SESSION['role']) && $_SESSION['role'] === 'consultant'){
-                    header("Location: account.html");
+                    header("Location: account.php");
                     exit();
                 } else {
-                    header("Location: account.html");
+                    header("Location: account.php");
                     exit();
                 }
                 exit();
