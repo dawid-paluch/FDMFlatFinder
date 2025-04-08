3480836135d77c15356009946dd5fdb42cf762d1
RevisionAaron <aaronparwany.p@gmail.com>
2025-04-08 02:26:58 +0100

    change account.html to account.php, checks for user log in to access details

---
+++ account.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link rel="stylesheet" href="css/reset.css" />
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="css/account.css"/>
+  
+  <title>FDM Flat Finder</title>
+</head>
+
+<body>
+    <header>
+        <div id="header">
+        <div id="logo">
+            <a href="mainpage.html">FlatFinderFDM</a>
+        </div>
+
+        <div id="nav">
+            <nav>
+                <a href="mainpage.html">Home</a>
+                <a href="about.html">About</a>
+                <a href="contact.html">Contact</a>
+                <a href="loginConsultant.html">Login</a>
+                <a href="account.php">My Account</a>
+            </nav>
+        </div>
+        </div>
+    </header>
+
+    <img id="background-image"src="images/homepage-stock-photo.jpg" alt="property photo" />
+
+    <div id="main">
+        <?php if (isset($_SESSION['id'])): ?>
+                <div id="formContainer">
+                    <h1>
+                        Your Personal Details
+                    </h1>
+                    <div id="detailtype-buttons">
+                        <a id="email-button" href="changeEmail.html">Change Email</a>
+
+                        <a id="password-button" href="changePassword.html">Change Password</a>
+
+                        <a id="username-button" href="changeUsername.html">Change Username</a>
+
+                        <a id="location-button" href="changeLocation.html">Change Location</a>
+                        
+                        <a id="age-button" href="changeAge.html">Change Age</a>
+                </div>
+        <?php else: ?>
+            <div id="formContainer">
+                    <h1>
+                        You must be logged in to access your account details
+                    </h1>
+            </div>
+        <?php endif; ?>
+    </div>
+</body>
+</html>
